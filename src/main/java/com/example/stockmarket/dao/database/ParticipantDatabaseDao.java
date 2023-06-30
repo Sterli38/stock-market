e64085883458996d2c69ba7e927edbedd3f56938
lenya <lenya.vel@mail.ru>
2023-06-30 09:24:15 +0300

    Изменили методы dao

--- src/main/java/com/example/stockmarket/dao/database/ParticipantDatabaseDao.java
+++ src/main/java/com/example/stockmarket/dao/database/ParticipantDatabaseDao.java
@@ -22,17 +22,20 @@ public class ParticipantDatabaseDao implements ParticipantDao {
 
     @Override
     public Participant createParticipant(@Nullable Participant participant) {
-        String sql = "INSERT INTO participant(name, creation_date, password) values(?, ?, ?)";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, participant.getName());
-            ps.setDate(2, new Date(participant.getCreationDate().getTime()));
-            ps.setString(3, participant.getPassword());
-            return ps;
-        }, keyHolder);
-        Participant returnParticipant = getParticipantById(keyHolder.getKey().longValue());
-        return returnParticipant;
+//        String sql = "INSERT INTO participant(name, creation_date, password) values(?, ?, ?)";
+//        KeyHolder keyHolder = new GeneratedKeyHolder();
+//        jdbcTemplate.update(connection -> {
+//            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+//            ps.setString(1, participant.getName());
+//            ps.setDate(2, new Date(participant.getCreationDate().getTime()));
+//            ps.setString(3, participant.getPassword());
+//            return ps;
+//        }, keyHolder);
+//        Participant returnParticipant = getParticipantById(keyHolder.getKey().longValue());
+//        return returnParticipant;
+        String sql = "INSERT INTO participant(name, creation_date, password) values(?, ?, ?) RETURNING *";
+      return jdbcTemplate.queryForObject(sql, new ParticipantMapper(), participant.getName(), participant.getCreationDate(), participant.getPassword());
+
     }
 
     @Override
@@ -48,25 +51,30 @@ public class ParticipantDatabaseDao implements ParticipantDao {
 
     @Override
     public Participant editParticipant(Participant participant) {
-        String sql = "UPDATE participant SET name = ?, creation_date = ?, password = ? WHERE id = ?";
-        KeyHolder keyHolder = new GeneratedKeyHolder();
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, participant.getName());
-            ps.setDate(2, new Date(participant.getCreationDate().getTime()));
-            ps.setString(3, participant.getPassword());
-            ps.setLong(4, participant.getId());
-            return ps;
-        }, keyHolder);
-        jdbcTemplate.update(sql, participant.getName(), participant.getCreationDate(), participant.getPassword(), participant.getId());
-        return getParticipantById(keyHolder.getKey().longValue());
+//        String sql = "UPDATE participant SET name = ?, creation_date = ?, password = ? WHERE id = ?";
+//        KeyHolder keyHolder = new GeneratedKeyHolder();
+//        jdbcTemplate.update(connection -> {
+//            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+//            ps.setString(1, participant.getName());
+//            ps.setDate(2, new Date(participant.getCreationDate().getTime()));
+//            ps.setString(3, participant.getPassword());
+//            ps.setLong(4, participant.getId());
+//            return ps;
+//        }, keyHolder);
+//        jdbcTemplate.update(sql, participant.getName(), participant.getCreationDate(), participant.getPassword(), participant.getId());
+//        return getParticipantById(keyHolder.getKey().longValue());
+        String sql = "UPDATE participant SET name = ?, creation_date = ?, password = ? WHERE id = ? RETURNING *";
+        return jdbcTemplate.queryForObject(sql, new ParticipantMapper(), participant.getName(), participant.getCreationDate(), participant.getPassword(), participant.getId());
+
     }
 
     @Override
     public Participant deleteParticipantById(long id) {
-        Participant returnParticipant = getParticipantById(id);
-        String sql = "DELETE FROM participant WHERE id = ?";
-        jdbcTemplate.update(sql, id);
-        return returnParticipant;
+//        Participant returnParticipant = getParticipantById(id);
+//        String sql = "DELETE FROM participant WHERE id = ?";
+//        jdbcTemplate.update(sql, id);
+//        return returnParticipant;
+        String sql = "DELETE FROM participant WHERE id = ? RETURNING *";
+        return jdbcTemplate.queryForObject(sql, new ParticipantMapper(), id);
     }
 }
