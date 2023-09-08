f88350015113e60315470cfdc3c0e47c64e56901
lenya <lenya.vel@mail.ru>
2023-09-08 11:54:30 +0300

    Не нашли изменений, добавили ExternalServiceException

--- src/main/java/com/example/stockmarket/controller/handler/ExceptionHandler.java
+++ src/main/java/com/example/stockmarket/controller/handler/ExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.example.stockmarket.controller.handler;
 
 import com.example.stockmarket.controller.response.ErrorResponse;
+import com.example.stockmarket.exception.ExternalServiceException;
 import com.example.stockmarket.exception.UserException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,5 +15,12 @@ public class ExceptionHandler {
         ErrorResponse errorResponse = new ErrorResponse();
         errorResponse.setMessage(exception.getMessage());
         return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler({ExternalServiceException.class})
+    public ResponseEntity<ErrorResponse> handleExternalServiceException(ExternalServiceException exception) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage("Внутренняя ошибка сервера, попробуйте позже");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
