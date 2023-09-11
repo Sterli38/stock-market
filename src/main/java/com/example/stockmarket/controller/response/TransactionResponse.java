9c8ac5ce645868d097a9ab581ef7d9cbe419ecb1
lenya <lenya.vel@mail.ru>
2023-09-11 18:42:45 +0300

    В ответе теперь показывается только id транзакции без null полей

--- src/main/java/com/example/stockmarket/controller/response/TransactionResponse.java
+++ src/main/java/com/example/stockmarket/controller/response/TransactionResponse.java
@@ -2,10 +2,12 @@ package com.example.stockmarket.controller.response;
 
 import com.example.stockmarket.entity.OperationType;
 import com.example.stockmarket.entity.Participant;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.Date;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class TransactionResponse {
     private Long id;
@@ -16,5 +18,5 @@ public class TransactionResponse {
     private String givenCurrency;
     private Double givenAmount;
     private Participant participant;
-    private double commission;
+    private Double commission;
 }
