4cc455f4cd993d5ff8eca26c62876c0d1ba7c525
lenya <lenya.vel@mail.ru>
2023-09-21 17:19:30 +0300

    fix

--- src/main/java/com/example/stockmarket/config/ApplicationJsonConfig.java
+++ src/main/java/com/example/stockmarket/config/ApplicationJsonConfig.java
@@ -1,15 +1,12 @@
 package com.example.stockmarket.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
-public class ApplicationObjectMapper {
+public class ApplicationJsonConfig {
     @Bean
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
         return new Jackson2ObjectMapperBuilder().serializationInclusion(JsonInclude.Include.NON_NULL);
