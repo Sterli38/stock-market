8ca44ec38a002cf77576c076af637dbaa00a28cd
lenya <lenya.vel@mail.ru>
2023-09-21 17:07:56 +0300

    fix

--- src/main/java/com/example/stockmarket/config/ApplicationObjectMapper.java
+++ src/main/java/com/example/stockmarket/config/ApplicationObjectMapper.java
@@ -2,16 +2,16 @@ package com.example.stockmarket.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 
 @Configuration
 public class ApplicationObjectMapper {
     @Bean
-    @Primary
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper()
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
+        return new Jackson2ObjectMapperBuilder().serializationInclusion(JsonInclude.Include.NON_NULL);
     }
 }
